d9bb867f294051eb5a819edfd606fda29d92d346
Sahrul Yoyo <111233171+sahrulyo@users.noreply.github.com>
2024-03-02 07:54:15 +0700

    sukses semua

--- Pekan 2/formative-9/main.go
+++ Pekan 2/formative-9/main.go
@@ -5,35 +5,20 @@ import (
 	"formative-9/libraries"
 )
 
-func Soal1() {
-	libraries.Soal1()
-	segitiga := segitigaSamaSisi{alas: 5, tinggi: 4}
-	persegi := persegiPanjang{panjang: 6, lebar: 3}
-
-	var bangunDatar hitungBangunDatar
-	bangunDatar = segitiga
-	fmt.Println("Luas Segitiga:", bangunDatar.luas())
-	fmt.Println("Keliling Segitiga:", bangunDatar.keliling())
-
-	bangunDatar = persegi
-	fmt.Println("Luas Persegi:", bangunDatar.luas())
-	fmt.Println("Keliling Persegi:", bangunDatar.keliling())
-
-	tabung := tabung{jariJari: 3, tinggi: 7}
-	balok := balok{panjang: 4, lebar: 5, tinggi: 6}
-
-	var bangunRuang hitungBangunRuang
-	bangunRuang = tabung
-	fmt.Println("Volume Tabung:", bangunRuang.volume())
-	fmt.Println("Luas Permukaan Tabung:", bangunRuang.luasPermukaan())
-
-	bangunRuang = balok
-	fmt.Println("Volume Balok:", bangunRuang.volume())
-	fmt.Println("Luas Permukaan Balok:", bangunRuang.luasPermukaan())
-
-}
-
 func main() {
 	libraries.SayHello()
+	libraries.Soal1()
+	libraries.Soal3()
+	libraries.Soal4()
 
+}
+func Soal2() {
+	myPhone := libraries.Phone{
+		Name:   "iPhone 13",
+		Brand:  "Apple",
+		Year:   2021,
+		Colors: []string{"Black", "White", "Blue"},
+	}
+
+	fmt.Println(myPhone.Display())
 }
