4215aad35b9265c357f8f885eab0b6640012db90
Sahrul Yoyo <111233171+sahrulyo@users.noreply.github.com>
2024-03-08 18:53:52 +0700

    updateformative-14

--- Pekan 3/formative-14/main.go
+++ Pekan 3/formative-14/main.go
@@ -44,6 +44,9 @@ func main() {
 
 	fmt.Println("succesfully connected to database")
 	CreateEmployee()
+	GetEmployees()
+	UpdateEmployee()
+	DeleteEmployee()
 
 }
 
@@ -68,6 +71,65 @@ func CreateEmployee() {
 		fmt.Println("Email already exists in the database, no data inserted")
 
 	}
+}
+
+func GetEmployees() {
+	var results = []Employee{}
+	sqlStatement := `SELECT * from employees`
+	rows, err := db.Query(sqlStatement)
+
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var employee = Employee{}
+
+		err = rows.Scan(&employee.ID, &employee.Full_name, &employee.Email, &employee.Age, &employee.Division)
+
+		if err != nil {
+			panic(err)
+		}
+		results = append(results, employee)
+	}
+	fmt.Println("Employee datas:", results)
+
+}
+
+func UpdateEmployee() {
+	sqlStatement := `
+        UPDATE employees
+        SET full_name = $2, email = $3, division = $4, age = $5
+        WHERE id = $1;`
+
+	res, err := db.Exec(sqlStatement, 1, "Airell Jordan Hidayat", "airellhidayat@gmail.com", "CurDevs", 24)
+	if err != nil {
+		panic(err)
+	}
+
+	count, err := res.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Jumlah data yang diupdate:", count)
+}
+
+func DeleteEmployee() {
+	sqlStatement := `
+	DELETE from employees
+	WHERE id = $1;`
+
+	res, err := db.Exec(sqlStatement, 1)
+	if err != nil {
+		panic(err)
+	}
+
+	count, err := res.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Deleted data amount:", count)
 }
 
 /*CREATE TABLE employees (
