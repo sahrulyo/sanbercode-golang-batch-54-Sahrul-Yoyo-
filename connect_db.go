6fee509117450397228778ac703b00eb86ed4148
Sahrul Yoyo <111233171+sahrulyo@users.noreply.github.com>
2024-03-08 11:43:32 +0700

    formative-14

---
+++ connect_db.go
@@ -0,0 +1,80 @@
+/*package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "user"
+	password = "Ulyasar10389#"
+	dbname   = "user"
+)
+
+type Employee struct {
+	ID        int
+	Full_name string
+	Email     string
+	Age       int
+	Division  string
+}
+
+var (
+	db  *sql.DB
+	err error
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic((err))
+	}
+
+	fmt.Println("succesfully connected to database")
+	CreateEmployee()
+
+}
+
+func CreateEmployee() {
+	var employee = Employee{}
+
+	sqlStatement := `
+	INSERT INTO employees (full_name, email, age, division)
+	VALUES ($1, $2, $3, $4)
+	ON CONFLICT (email) DO NOTHING
+	Returning * `
+
+	err = db.QueryRow(sqlStatement, "Airell Jordan", "airell@gmail.com", 23, "IT").
+		Scan(&employee.ID, &employee.Full_name, &employee.Email, &employee.Age, &employee.Division)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("No duplicate email found, data inserted successfully")
+		} else {
+			panic(err)
+		}
+	} else {
+		fmt.Println("Email already exists in the database, no data inserted")
+
+	}
+}
+
+/*CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(50) NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    age INT NOT NULL,
+    division VARCHAR(20) NOT NULL
+); */
+*/
--- import.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "belajar-golang-level-akses/library"
-
-func main() {
-	library.SayHello("Yoyo")
-}
